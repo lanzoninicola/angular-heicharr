4fa2afc2d9c1cd99a6d4914e99ecc2d41f4d6778
Nicola <lanzoni.nicola@gmail.com>
2022-03-13 18:33:23 -0300

    Add env variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  API: 'https://heicharr-be-mock.herokuapp.com',
+  auth: {
+    domain: '',
+    clientId: '',
+    redirectUri: window.location.origin,
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  API: 'https://heicharr-be-mock.herokuapp.com',
+  auth: {
+    domain: 'heicharr.eu.auth0.com',
+    clientId: 'lVUFh9qNXCmcdrJdPrgKnH1EI8gU3IOU',
+    redirectUri: window.location.origin,
+  },
+};

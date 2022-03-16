c90cbd2b4db3d5306443d7ab90be760d8ddfdb86
Nicola <lanzoni.nicola@gmail.com>
2022-03-16 12:59:52 -0300

    Move CandidateIdBadgeComponent inside the CandidatesModule

--- src/app/pages/candidates/components/candidate-id-badge/candidate-id-badge.component.ts
+++ src/app/pages/candidates/components/candidate-id-badge/candidate-id-badge.component.ts
@@ -2,11 +2,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CandidateModel } from 'src/app/pages/candidates/models/candidate.model';
 
 @Component({
-  selector: 'app-candidate-id-badge',
+  selector: 'ahr-candidate-id-badge',
   template: `
     <ahr-profile-badge [profileData]="profileData"></ahr-profile-badge>
   `,
-  styleUrls: ['./candidate-id-badge.component.scss'],
 })
 export class CandidateIdBadgeComponent implements OnInit {
   @Input()
